816cd9fcd16d765004e98f81580b386fd57ee4ff
chris <chris.bishop155@gmail.com>
2022-10-23 22:28:30 +0100

    Added email config

--- lib/oas.ex
+++ lib/oas.ex
@@ -20,4 +20,15 @@ defmodule Oas do
     Oas.Repo.insert(toSave)
   end
 
+  def testEmail do
+    email = Swoosh.Email.new()
+    |> Swoosh.Email.to({"chris", "chrisjbishop155@hotmail.com"})
+    |> Swoosh.Email.from({"chris", "chris@oxfordshireacrosociety.co.uk"})
+    |> Swoosh.Email.subject("Hello, Avengers!")
+    # |> Swoosh.Email.html_body("<h1>Hello World</h1>")
+    |> Swoosh.Email.text_body("Hello World\n")
+
+    Oas.Mailer.deliver(email)
+  end
+
 end
