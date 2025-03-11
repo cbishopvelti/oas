08f84803924d98f6fd32e0ad16b12efee5242b19
chris <chrisjbishop155@gmail.com>
2025-03-11 05:22:34 +0000

    Default for attendance value.

--- lib/oas/config/tokens.ex
+++ lib/oas/config/tokens.ex
@@ -1,3 +1,5 @@
+import Ecto.Query, only: [from: 2]
+
 defmodule Oas.Config.Tokens do
   use Ecto.Schema
 
@@ -5,5 +7,12 @@ defmodule Oas.Config.Tokens do
     field :quantity, :integer
     field :value, :decimal
     timestamps()
+  end
+
+  def get_min_token do
+    from(ct in Oas.Config.Tokens,
+      order_by: [asc: :value],
+      limit: 1
+    ) |> Oas.Repo.one!()
   end
 end
