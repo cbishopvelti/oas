197d3270fd27b66c5fa83d6092240dad5e4d3247
cbishopvelti <chrisjbishop155@gmail.com>
2025-11-09 21:19:08 +0000

    Can now add member to a membership period.

--- oas-web/src/MembershipPeriod/MembershipPeriodMembers.js
+++ oas-web/src/MembershipPeriod/MembershipPeriodMembers.js
@@ -1,12 +1,15 @@
 import { gql, useQuery, useMutation } from "@apollo/client"
 import { MembersDisplay } from "../Member/MembersDisplay"
-import { IconButton
+import {
+  IconButton, Dialog, DialogTitle, FormControl,
+  Autocomplete, TextField, Button
  } from "@mui/material";
-import { get, reduce } from 'lodash';
+import { differenceBy, differenceWith, get, reduce } from 'lodash';
 import { useParams, useOutletContext, Link } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
 import PaidIcon from '@mui/icons-material/Paid';
+import CardMembershipIcon from '@mui/icons-material/CardMembership';
 
 
 export const DeleteMembership = ({membership_period_id, membership_period_name, refetch}) => {
@@ -38,19 +41,82 @@ export const DeleteMembership = ({membership_period_id, membership_period_name, 
       }}>
         <DeleteIcon sx={{color: 'red'}} />
       </IconButton>
-      
+
     </>
   }
+}
+
+const AddMembership = ({membership_period_id, members: existing_members, changeNo, setChangeNo}) => {
+  const [open, setOpen] = useState(false);
+  const [member, setMember] = useState({});
+
+  let { data, refetch: refetchMembers } = useQuery(gql`query {
+    members {
+      id,
+      name
+    }
+  }`);
+  let members = get(data, 'members', []);
+  members = differenceBy(members, existing_members, ({id}) => { return id })
+
+  let [mutate] = useMutation(gql`
+    mutation($member_id: Int!, $membership_period_id: Int!) {
+      add_membership(member_id: $member_id, membership_period_id: $membership_period_id) {
+        success
+      }
+    }
+  `)
+  const addMemberClick = async () => {
+    await mutate({
+      variables: {
+        member_id: member.member_id,
+        membership_period_id
+      }
+    })
+    setChangeNo(changeNo + 1)
+    setOpen(false)
+  }
+
+  return <>
+      <IconButton title={`Add member`} onClick={() => setOpen(true)}>
+        <CardMembershipIcon />
+      </IconButton>
+      <Dialog open={open} onClose={() => {setOpen(false)}}>
+        <DialogTitle>Select member to add</DialogTitle>
+        <FormControl sx={{m: 2, minWidth: 256}}>
+          <Autocomplete
+            id="member"
+            required
+            value={member.member_name || null}
+            isOptionEqualToValue={(a, b) => {
+              return a.label === b
+            }}
+            options={members.map(({name, id}) => ({label: name, member_id: id }))}
+            renderInput={(params) => <TextField {...params} required label="Who" />}
+            onChange={(event, newValue, a, b, c, d) => {
+              setMember({
+                member_id: newValue.member_id,
+                member_name: newValue.label
+              })
+            }}
+            />
+        </FormControl>
+        <FormControl sx={{m: 2}}>
+          <Button onClick={addMemberClick}>Add to membership period</Button>
+        </FormControl>
+      </Dialog>
+    </>
 }
 
 
 export const MembershipPeriodMembers = () => {
   const { setTitle } = useOutletContext();
+  const [changeNo, setChangeNo] = useState(0);
 
   let { id } = useParams();
   id = parseInt(id);
 
-  
+
 
   const {data, refetch} = useQuery(gql`
     query ($id: Int!) {
@@ -79,7 +145,7 @@ export const MembershipPeriodMembers = () => {
   })
   useEffect(() => {
     refetch();
-  }, [])
+  }, [changeNo])
 
   const members = get(data, "membership_period.memberships", []).map(({member}) => member);
 
@@ -111,5 +177,8 @@ export const MembershipPeriodMembers = () => {
 
   const memberships = get(data, 'membership_period.memberships', [])
 
-  return <MembersDisplay data={memberships} dataKey={`member`} ExtraActions={DeleteMembership({membership_period_id: id, membership_period_name: get(data, 'membership_period.name', id), refetch})}/>
+  return <>
+    <AddMembership members={members} membership_period_id={id} changeNo={changeNo} setChangeNo={setChangeNo} />
+    <MembersDisplay data={memberships} dataKey={`member`} ExtraActions={DeleteMembership({membership_period_id: id, membership_period_name: get(data, 'membership_period.name', id), refetch})}/>
+  </>
 }
