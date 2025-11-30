1c81f6787534c27e6565f070e139fb8bc330f0b5
chris <chrisjbishop155@gmail.com>
2025-11-30 00:53:19 +0000

    Added recurring events.

--- lib/oas/backup_job.ex
+++ lib/oas/backup_job.ex
@@ -8,23 +8,23 @@ defmodule Oas.BackupJob do
   def init(state) do
     schedule()
 
-    
+
     {:ok, state}
   end
 
   def handle_info(:backup, state) do
     # the actual work to be done
     IO.puts("Running backup ...")
-    
+
     # work is done, let's schedule again
     Oas.Repo.backup()
 
     IO.puts("EO Running backup")
 
-    
+
     {:noreply, state}
   end
-  
+
   defp schedule() do
     # schedule after one minute
     # Process.send_after(self(), :backup, 120_000)
