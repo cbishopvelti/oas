f98475947c495beb874797cad51242330f85eeb5
cbishopvelti <chrisjbishop155@gmail.com>
2026-01-07 18:09:15 +0000

    Fixed gocardless auth failure.

--- lib/oas/gocardless/trans_server.ex
+++ lib/oas/gocardless/trans_server.ex
@@ -37,46 +37,53 @@ defmodule Oas.Gocardless.TransServer do
     #   {"http_x_ratelimit_account_success_reset", "86400"}
     # ]
     # Process.sleep(12_000)
-    {:ok, headers} = Oas.Gocardless.Transactions.process_transacitons() # DEBUG ONLY, uncomment
+
+    case Oas.Gocardless.Transactions.process_transacitons() do
+      {:ok, headers} ->
+        remaining = List.keyfind!(headers, "http_x_ratelimit_account_success_remaining", 0) |> elem(1) |> String.to_integer()
+        reset_seconds = List.keyfind!(headers, "http_x_ratelimit_account_success_reset", 0)
+          |> elem(1)
+          |> String.to_integer()
+          |> Kernel.+(60) # Insure we have a little buffer.
+
+        timeout = div(reset_seconds, (remaining + 1))
+        timeout = if Mix.env() != :test do
+          Enum.max([timeout, 3_600])
+        else
+          timeout
+        end
+        _timeout_ms = timeout * 1000
+        # timer_ref = Process.send_after(self(), :init, timeout_ms)
 
 
-    remaining = List.keyfind!(headers, "http_x_ratelimit_account_success_remaining", 0) |> elem(1) |> String.to_integer()
-    reset_seconds = List.keyfind!(headers, "http_x_ratelimit_account_success_reset", 0)
-      |> elem(1)
-      |> String.to_integer()
-      |> Kernel.+(60) # Insure we have a little buffer.
+        rerun_in_ms = if (remaining > 0) do
+          london_dt = DateTime.now!("Europe/London")
+          local_time = DateTime.to_time(london_dt)
 
-    timeout = div(reset_seconds, (remaining + 1))
-    timeout = if Mix.env() != :test do
-      Enum.max([timeout, 3_600])
-    else
-      timeout
+          Time.diff(Time.from_iso8601!("09:30:00"), local_time, :millisecond)
+          |> Integer.mod(
+             86_400_000
+          )
+        else
+          reset_seconds * 1000
+        end
+
+        Absinthe.Subscription.publish(OasWeb.Endpoint, %{}, gocardless_trans_status: "*")
+
+        # Once a day
+        timer_ref = Process.send_after(self(), :init, rerun_in_ms)
+        {:noreply,
+          %{
+            timer_ref: timer_ref
+          }
+        }
+      {:unauth_401, error} ->
+        Logger.error("Gocardless unathenticated #{error}")
+        Oas.Gocardless.send_warning(:gocardless_get_accounts, error)
+        {:stop, :normal, %{timer_ref: nil}}
     end
-    _timeout_ms = timeout * 1000
-    # timer_ref = Process.send_after(self(), :init, timeout_ms)
 
 
-    rerun_in_ms = if (remaining > 0) do
-      london_dt = DateTime.now!("Europe/London")
-      local_time = DateTime.to_time(london_dt)
-
-      Time.diff(Time.from_iso8601!("09:30:00"), local_time, :millisecond)
-      |> Integer.mod(
-         86_400_000
-      )
-    else
-      reset_seconds * 1000
-    end
-
-    Absinthe.Subscription.publish(OasWeb.Endpoint, %{}, gocardless_trans_status: "*")
-
-    # Once a day
-    timer_ref = Process.send_after(self(), :init, rerun_in_ms)
-    {:noreply,
-      %{
-        timer_ref: timer_ref
-      }
-    }
   end
 
   @impl true
